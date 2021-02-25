4d46c312688b8912feda83c22ed3bd32c40c8891
hiromu_saito <hiromu.saito@bigtreetc.com>
2021-02-26 00:13:44 +0900

    create Header

--- ec-front/src/App.vue
+++ ec-front/src/App.vue
@@ -1,8 +1,18 @@
 <template>
   <div id="app">
+    <Header></Header>
+    <hr />
     <div><router-link to="/">Home</router-link></div>
     <router-view />
   </div>
 </template>
-
+<script>
+import Header from "@/global/Header";
+export default {
+  name: "App",
+  components: {
+    Header,
+  },
+};
+</script>
 <style>
---
+++ ec-front/src/global/Header.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <span>EC-Sample</span>
+    <span style="float: right">
+      <span style="padding-right: 10px"> {{ date }} {{ time }} </span>
+      <!-- ログイン後はユーザー名は表示されるようにする -->
+      こんにちは，ゲストさん</span
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  data() {
+    return {
+      date: "",
+      time: "",
+    };
+  },
+  mounted() {
+    setInterval(this.updateTime, 1000);
+  },
+  methods: {
+    updateTime() {
+      const now = new Date();
+      this.date =
+        now.getFullYear() +
+        "/" +
+        String(now.getMonth() + 1).padStart(2, "0") +
+        "/" +
+        String(now.getDate()).padStart(2, "0");
+
+      this.time =
+        String(now.getHours()).padStart(2, "0") +
+        ":" +
+        String(now.getMinutes()).padStart(2, "0") +
+        ":" +
+        String(now.getSeconds()).padStart(2, "0");
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
