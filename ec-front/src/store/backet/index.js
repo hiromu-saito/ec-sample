2d8ee0a3fce58bb288b14ad8eb849752e725cc4c
hiromu.saito <hiromu.saito@bigtreetc.com>
2021-03-07 00:07:20 +0900

    WIP display account in backet

--- ec-front/src/store/backet/index.js
+++ ec-front/src/store/backet/index.js
@@ -3,46 +3,29 @@ const state ={
 }
 
 const mutations = {
-  // setItem(state,payload){
-  //   state.results = payload
-  // },
-  // resetResults(state){
-  //   state.results = undefined
-  // }
-  
   addItem(state,payload){
-  console.log('addItem')
-  console.log(payload)
-  //  payload.forEach(addItem => {
-  //    const index = state.backetItems.findIndex(backetItem =>{
-  //      console.log('find')
-  //      console.log('backetItem' + backetItem.code)
-  //      console.log('addItem' + addItem.code)
-  //      backetItem.code === addItem.code
-  //     })
-  //     console.log(index)
-  //    if(index === -1){
-  //      console.log('else')
-  //      state.backetItems.push(addItem)
-  //   }else{
-  //       console.log('if')
-  //       state.backetItems[index].count += addItem.count
-  //    }
-  //  })
-  state.backetItems = state.backetItems.concat(payload)
-}
+    payload.forEach(addItem => {
+      const index = state.backetItems.findIndex(backetItem =>{
+        return backetItem.code === addItem.code
+      })
+      if(index === -1){
+        addItem.count = parseInt(addItem.count,10)
+        state.backetItems.push(addItem)
+      }else{
+        state.backetItems[index].count += parseInt(addItem.count,10)
+      }
+    })
+  },
+  removeAllItem(state){
+    state.backetItems = []
+  }
 }
 const actions = {
-  // async setResultsAction({commit},){
-  //   const res = await fetch(searchUrl)
-  //   .then(response => response.json())
-  //   commit('setResults',res)
-  // },
-  // resetResultsAction({commit}){
-  //   commit('resetResults')
-  // }
   addItemAction({commit},payload){
     commit('addItem',payload)
+  },
+  removeAllItemAction({commit}){
+    commit('removeAllItem')
   }
 }
 
--- ec-front/src/views/Backet.vue
+++ ec-front/src/views/Backet.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>お買い物かご</h1>
-    <div class="backet">
+    <div class="backet" v-if="!isChecking && !orderSuccess">
       <table>
         <thead>
           <tr>
@@ -21,17 +21,73 @@
             <td>{{backetItem.code}}</td>
             <td>{{backetItem.name}}</td>
             <td>{{backetItem.distributor}}</td>
-            <td>{{backetItem.price}}</td>
-            <td>{{backetItem.count}}</td>
+            <td>&yen;{{backetItem.price}}</td>
+            <!-- <td>{{backetItem.count}}</td> -->
+            <td>
+              <input 
+                type="text"
+                v-model="backetItem.count"
+              />
+                </td>
           </tr>
         </tbody>
       </table>
       <p>
         <button>取り消し</button>
-        <button>買い物をやめる</button>
-        <button>注文する</button>
+        <button @click="stopShopping">買い物をやめる</button>
+        <button @click="check">注文する</button>
         <button @click="$router.push('/')">メニューへ</button>
       </p>
+    </div>
+    <div class="backet_checking" v-if="isChecking && !orderSuccess">
+      〇商品一覧
+      <table>
+        <thead>
+          <tr>
+            <th>商品コード</th>
+            <th>商品名</th>
+            <th>販売元</th>
+            <th>価格</th>
+            <th>購入数</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr 
+            v-for="backetItem in backetItems"
+            :key="backetItem.code">
+            <td>{{backetItem.code}}</td>
+            <td>{{backetItem.name}}</td>
+            <td>{{backetItem.distributor}}</td>
+            <td>&yen;{{backetItem.price}}</td>
+            <td>{{backetItem.count}}</td>
+          </tr>
+        </tbody>
+      </table>
+      〇料金
+      <table>
+        <tr>
+          <th>小計</th>
+          <td>&yen;{{subtotal}}</td>
+        </tr>
+        <!-- <tr>
+          <th>消費税</th>
+          <td>&yen;{{tax}}</td>
+        </tr>
+        <tr>
+          <th>合計金額</th>
+          <td>&yen;{{total}}</td>
+        </tr> -->
+      </table>
+      <p>
+        <button @click="stopShopping">買い物をやめる</button>
+        <button @click="order">注文する</button>
+        <button @click="isChecking = false">戻る</button>
+      </p>
+    </div>
+    <div v-if="orderSuccess">
+      注文が完了しました。
+      <br>
+      <button @click="$router.push('/')">メニューへ戻る</button>
     </div>
   </div>
 </template>
@@ -39,11 +95,55 @@
 
 <script>
 import store from '../store'
+import {mapActions} from 'vuex'
+
+const BACKET = store.state.backet
 
 export default {
   name: "Backet",
+  data(){
+    return{
+      isChecking:false,
+      orderSuccess:false
+    }
+  },
   computed:{
-    backetItems:()=> store.state.backet.backetItems
+    backetItems:()=> BACKET.backetItems,
+    // account:()=>{
+    //   let test = this.backetItems
+    //   console.log(test)
+    //   let subtotal  = 0
+    //   BACKET.backetItems.forEach(backetItem => {
+    //     subtotal += parseInt(backetItem.price,10)
+    //   });
+    //   const tax = subtotal * 0.1
+    //   return {
+    //     subtotal,
+    //     tax,
+    //     total:subtotal + tax
+    //   }
+    // },
+    subtotal:()=>{
+      let subtotal = 0;
+      BACKET.backetItems.forEach(backetItem => {
+        subtotal += parseInt(backetItem.price,10)
+      })
+      return subtotal
+    },
+  },
+  methods:{
+    ...mapActions('backet',['removeAllItemAction']),
+    stopShopping(){
+      this.removeAllItemAction()
+      this.$router.push('/')
+    },
+    check(){
+      //TODO 在庫確認
+      this.isChecking = true
+    },
+    order(){
+      this.orderSuccess = true
+    }
   }
 };
 </script>
@@ -68,6 +168,27 @@ export default {
 }
 
 .backet button {
+  margin-right: 10px;
+}
+.backet_checking {
+  margin-left: 32px;
+}
+.backet_checking table,
+.backet_checking th,
+.backet_checking td {
+  border-collapse: collapse;
+  border: solid 1px #000;
+}
+.backet_checking th {
+  background-color: pink;
+  text-align: right;
+}
+.backet_checking td {
+  vertical-align: middle;
+
+}
+
+.backet_checking  button {
   margin-right: 10px;
 }
 </style>
